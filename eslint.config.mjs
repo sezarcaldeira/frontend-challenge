69dbbeef84dca7727d24af750c61f15aeed71c81
Sezar Caldeira <sezar.caldeira@gmail.com>
2025-05-22 14:23:48 -0300

    chore: config eslint

---
+++ eslint.config.mjs
@@ -0,0 +1,28 @@
+import js from "@eslint/js";
+import globals from "globals";
+import pluginReact from "eslint-plugin-react";
+import { defineConfig } from "eslint/config";
+
+export default defineConfig([
+  {
+    files: ["src/*.{js,mjs,cjs,jsx}"],
+    plugins: { js },
+    extends: ["js/recommended"],
+  },
+  {
+    files: ["**/*.{js,mjs,cjs,jsx}"],
+    languageOptions: { globals: globals.browser },
+  },
+  {
+    files: ["src/*.{js,mjs,cjs,jsx}"],
+    extends: [pluginReact.configs.flat.recommended],
+    rules: {
+      "react/react-in-jsx-scope": "off",
+    },
+    settings: {
+      react: {
+        version: "detect",
+      },
+    },
+  },
+]);
--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,8 +1,8 @@
 import React from "react";
-import ReactDOM from "react-dom";
 import { createRoot } from "react-dom/client";
 import App from "./App";
 
+// eslint-disable-next-line no-undef
 it("renders without crashing", () => {
   const div = document.createElement("div");
   const root = createRoot(div);
